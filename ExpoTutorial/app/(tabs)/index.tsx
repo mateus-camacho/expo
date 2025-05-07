6250357f6dfc523145a749a76616ef84ed58b7de
Mateus Camacho <teukuka.silva@hotmail.com>
2025-05-06 22:23:17 -0300

    aula2
    agora aula 2 kkkkk

--- ExpoTutorial/app/(tabs)/index.tsx
+++ ExpoTutorial/app/(tabs)/index.tsx
@@ -1,14 +1,24 @@
 import { Link } from 'expo-router';
 import { Text, View,  StyleSheet } from 'react-native';
+import { Image } from 'expo-image';
+import ImageViewer from '@/components/ImageView';
+import Button from '@/components/button';
+
+//imagem que vai ta quando aparecer inicialmente a tela
+const ImagemPadrao = require('@/assets/images/background-image.png')
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Home screen</Text>
-      <Link href="/about" style={styles.button}>
-        Ir pro about da pagina
-      </Link>
+      <View style={styles.imageConteiner}>
+        <ImageViewer imgSource={ImagemPadrao}/>
+      </View>
+      <View style={styles.footerContainer}>
+        <Button theme='primary' label='Escolha uma foto' />
+        <Button label='Use essa foto'/>
+      </View>
     </View>
+
   );
 }
 
@@ -17,14 +27,12 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#25292e',
     alignItems: 'center',
-    justifyContent: 'center',
+  },  
+  imageConteiner: {
+    flex: 1,
   },
-  text: {
-    color: '#fff',
-  },
-  button: {
-    fontSize: 20,
-    textDecorationLine: 'underline',
-    color: '#fff',
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
   },
 });
